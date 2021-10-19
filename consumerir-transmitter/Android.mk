LineageOS/android_device_leeco_msm8996-common

msm8996-common: implement binderized consumerir HAL + bridge
Since LeEco doesn't use any driver blobs for consumerir and instead
uses a quickset SDK, we need a service that acts as a bridge to the
quickset SDK for translating calls from our consumerir  HAL,  which
we named "ConsumerirTransmitter".

How this works is our consumerir HAL receives an IR command,  which
it  sends  to the ConsumerirTransmitter that translates it  to  the
quickset SDK.

Change-Id: I9e7eebf7071c06ec1e91cf67171b5aca7ae005d2

consumerir-transmiter/Android.mk
@@ -1,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := ConsumerirTransmiter
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_VENDOR_MODULE    := true
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))

consumerir-transmiter/src/org/lineageos/consumerirtransmiter/IControl.java
@@ -1,0 +1,80 @@
+package org.lineageos.consumerirtransmiter;
+
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+
+/**
+ * Service client class. Used to interact with the bound UEI QuickSet service.
+ * Package name doesn't matter, but class must be called "IControl"!!
+ */
+public class IControl {
+    public static final String ACTION = "com.uei.control.IControl";
+
+    /**
+     * This is the package name of the Leeco QuickSet service
+     */
+    public static final String QUICKSET_UEI_PACKAGE_NAME = "com.uei.quicksetsdk.letv";
+
+    /**
+     * This is the service class name within the Quickset Package.
+     */
+    public static final String QUICKSET_UEI_SERVICE_CLASS = "com.uei.control.Service";
+
+    private IBinder _controlService = null;
+
+    public IControl(IBinder service) {
+        this._controlService = service;
+    }
+
+    /**
+     * Get result code for last action from the QuickSet service.
+     *
+     * @return result code, 0 means OK
+     * @throws RemoteException
+     */
+    public int getLastResultcode() throws RemoteException {
+        Parcel _data = Parcel.obtain();
+        Parcel _reply = Parcel.obtain();
+        try {
+            _data.writeInterfaceToken(ACTION);
+            this._controlService.transact(13, _data, _reply, 0);
+            _reply.readException();
+            return _reply.readInt();
+        } finally {
+            _reply.recycle();
+            _data.recycle();
+        }
+    }
+
+    /**
+     * Transmit custom infrared pattern. Work like the official Android API in
+     * ConsumerIrManager.transmit().
+     *
+     * @param carrierFrequency carrier frequency
+     * @param pattern          IR pattern to send
+     * @return status
+     * @throws RemoteException
+     */
+    public int transmit(int carrierFrequency, int[] pattern) throws RemoteException {
+        Parcel _data = Parcel.obtain();
+        Parcel _reply = Parcel.obtain();
+        int _result = 1;
+        if (this._controlService != null) {
+            try {
+                _data.writeInterfaceToken(ACTION);
+                _data.writeInt(carrierFrequency);
+                _data.writeIntArray(pattern);
+                this._controlService.transact(18, _data, _reply, 0);
+                _reply.readException();
+                _result = _reply.readInt();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                _reply.recycle();
+                _data.recycle();
+            }
+        }
+        return _result;
+    }
+}

consumerir-transmiter/src/org/lineageos/consumerirtransmiter/Startup.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (c) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.consumerirtransmiter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.util.Log;
+
+public class Startup extends BroadcastReceiver {
+    private static final String TAG = "ConsumerirTransmiter";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent.getAction();
+        if (action.equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
+            Log.d(TAG, "Starting");
+            context.startServiceAsUser(new Intent(context, ConsumerirTransmiterService.class), UserHandle.CURRENT);
+        }
+    }
+}

Stealth1226/android_device_lge_msm8996-common

consumerir-transmitter/Android.mk
@@ -1,0 +1,19 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := ConsumerirTransmitter
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_VENDOR_MODULE    := true
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))

consumerir-transmitter/src/org/lineageos/consumerirtransmitter/IControl.java
@@ -1,0 +1,78 @@
+package org.lineageos.consumerirtransmitter;
+
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+
+/**
+ * Service client class. Used to interact with the bound UEI QuickSet service.
+ * Package name doesn't matter, but class must be called "IControl"!!
+ */
+public class IControl {
+    public static final String ACTION = "com.uei.control.IControl";
+
+    /**
+     * This is the package name of the Leeco QuickSet service
+     */
+    public static final String QUICKSET_UEI_PACKAGE_NAME = "com.uei.quicksetsdk.letv";
+
+    /**
+     * This is the service class name within the Quickset Package.
+     */
+    public static final String QUICKSET_UEI_SERVICE_CLASS = "com.uei.control.Service";
+
+    private IBinder _controlService = null;
+
+    public IControl(IBinder service) { this._controlService = service; }
+
+    /**
+     * Get result code for last action from the QuickSet service.
+     *
+     * @return result code, 0 means OK
+     * @throws RemoteException
+     */
+    public int getLastResultcode() throws RemoteException {
+        Parcel _data = Parcel.obtain();
+        Parcel _reply = Parcel.obtain();
+        try {
+            _data.writeInterfaceToken(ACTION);
+            this._controlService.transact(13, _data, _reply, 0);
+            _reply.readException();
+            return _reply.readInt();
+        } finally {
+            _reply.recycle();
+            _data.recycle();
+        }
+    }
+
+    /**
+     * Transmit custom infrared pattern. Work like the official Android API in
+     * ConsumerIrManager.transmit().
+     *
+     * @param carrierFrequency carrier frequency
+     * @param pattern          IR pattern to send
+     * @return status
+     * @throws RemoteException
+     */
+    public int transmit(int carrierFrequency, int[] pattern) throws RemoteException {
+        Parcel _data = Parcel.obtain();
+        Parcel _reply = Parcel.obtain();
+        int _result = 1;
+        if (this._controlService != null) {
+            try {
+                _data.writeInterfaceToken(ACTION);
+                _data.writeInt(carrierFrequency);
+                _data.writeIntArray(pattern);
+                this._controlService.transact(18, _data, _reply, 0);
+                _reply.readException();
+                _result = _reply.readInt();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                _reply.recycle();
+                _data.recycle();
+            }
+        }
+        return _result;
+    }
+}

consumerir-transmitter/src/org/lineageos/consumerirtransmitter/Startup.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (c) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.consumerirtransmitter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.util.Log;
+
+public class Startup extends BroadcastReceiver {
+    private static final String TAG = "ConsumerirTransmitter";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent.getAction();
+        if (action.equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
+            Log.d(TAG, "Starting");
+            context.startServiceAsUser(
+                new Intent(context, ConsumerirTransmitterService.class), UserHandle.CURRENT);
+        }
+    }
+}