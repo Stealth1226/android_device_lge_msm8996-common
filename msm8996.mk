06c9a9cec9d5538cefce9a87ae57ffba2f159b70
Stealth1226 <rishawniyer06@gmail.com>
2021-10-30 21:03:45 +0530

    msm8996-common: build keymaster 4.0

--- msm8996.mk
+++ msm8996.mk
@@ -182,7 +182,11 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0 \
     android.hardware.keymaster@3.0.vendor \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@4.0 \
+    android.hardware.keymaster@4.0.vendor \
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service
 
 # Gesture handler
 PRODUCT_PACKAGES += \
