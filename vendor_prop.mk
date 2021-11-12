6c827f07b79d6728aa4f5b8297188e87001f8eb8
Wilson Sung <wilsonsung@google.com>
2021-11-12 21:31:47 +0530

    msm8996-common: Set SOC manufacturer and chipset properties
    Bug: 158284209
    Bug: 180589452
    Change-Id: I26d4b3767d336321bd562a8d5a289ce864536f5e

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -221,6 +221,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sensors.wul_delay=3000 \
     persist.sensors.onhand.en=0
 
+# SoC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=Qualcomm
+    ro.soc.model=MSM8996
+
 # Storage.xml moment
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.binary_xml=false
