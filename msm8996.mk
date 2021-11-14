faf6faede72a7016a1065666f2d3e8dfc7fc8f23
Stealth1226 <rishawniyer06@gmail.com>
2021-11-14 19:13:06 +0530

    Revert "msm8996-common: Build a bunch of blobs alongside their vendor"
    This reverts commit fe2f71b21dcc56e80025858ecbec3c270e01ad8f.

--- msm8996.mk
+++ msm8996.mk
@@ -105,14 +105,8 @@ PRODUCT_PACKAGES += \
 
 -include $(LOCAL_PATH)/audio/config.mk
 
-# Biometrics
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1.vendor
-
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0 \
-    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
     hwaddrs \
@@ -162,11 +156,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3-service.clearkey
 
 # Recovery
 PRODUCT_PACKAGES += \
@@ -175,8 +165,7 @@ PRODUCT_PACKAGES += \
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service \
-    android.hardware.gatekeeper@1.0.vendor
+    android.hardware.gatekeeper@1.0-service
 
 # Gesture handler
 PRODUCT_PACKAGES += \
@@ -187,8 +176,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-om-lg
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0 \
-    android.hardware.gnss@1.0.vendor \
     android.hardware.gnss@1.0-impl-qti \
     android.hardware.gnss@1.0-service-qti \
     libgnss \
@@ -248,10 +235,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service \
-    android.hardware.keymaster@3.0.vendor \
     android.hardware.keymaster@4.0-impl \
-    android.hardware.keymaster@4.0-service \
-    android.hardware.keymaster@4.0.vendor
+    android.hardware.keymaster@4.0-service
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
@@ -269,7 +254,6 @@ PRODUCT_COPY_FILES += \
 
 # Net
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.0.vendor \
     android.system.net.netd@1.0 \
     libandroid_net \
     netutils-wrapper-1.0
@@ -331,13 +315,6 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     librmnetctl
 
-# Radio
-PRODUCT_PACKAGES += \
-    android.hardware.radio@1.1 \
-    android.hardware.radio@1.1.vendor \
-    android.hardware.radio.deprecated@1.0 \
-    android.hardware.radio.deprecated@1.0.vendor
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
@@ -356,7 +333,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 
-
+    
 # TimeKeep
 PRODUCT_PACKAGES += \
     timekeep \
